125cca8e6f94a23f6b56e68fbaba22ee23aac9d1
laribrito <lary.29.ds@gmail.com>
2024-05-11 00:43:38 -0300

    feat: change the class name

--- segundo bloco/2.mmqDiscreto/main.py
+++ segundo bloco/2.mmqDiscreto/main.py
@@ -1,7 +1,8 @@
 import sys
 import os
 
-import sympy as sy
+import numpy as np
+from sympy import Symbol, zeros
 
 # Obtém o diretório atual do script
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -11,39 +12,41 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import AjustPolinClass
 
-class InterpolacaoLagrange(AjustPolinClass.AjustPolin):
+class MMQ_Discreto(AjustPolinClass.AjustPolin):
     def __init__(self, valoresX: list, valoresY: list) -> None:
         super().__init__(valoresX, valoresY)
-    
-    def polinomioLagrange(self, index):
-        denominador = 1
-        strNumerador = ''
-
-        # numerador
-        for i in range(self.qtdPares):
-            if i != index:
-                strNumerador += f'(x - {self.allX[i]}) *'
-        strNumerador = strNumerador[:-1] 
-
-        # denominador
-        for i in range(self.qtdPares):
-            if i != index:
-                denominador *= self.allX[index] - self.allX[i]
-
-        return f'({strNumerador})/{denominador}'
 
     def resolve(self):
-        func = ''
-        x = sy.Symbol('x')
+        x = Symbol("x")
+        funcao = [1, eval("x"), eval("x**2")]
+        tamFuncao = len(funcao)
+        matrizUi = [self.qtdPares*[1]]
+        
+        listaAux = []
+        for i in range(tamFuncao):
+            aux = funcao[i]
+            if(aux != 1):
+                for elem in self.allX:
+                    listaAux.append(aux.subs(x, elem))
+                    
+                matrizUi.append(listaAux)
+                listaAux = []
 
-        for i in range(self.qtdPares):
-            func += f'{self.allY[i]}*({self.polinomioLagrange(i)}) +'
-        func = func[:-1]
+        vetorF = zeros(tamFuncao, 1)
+        matrizRes = zeros(tamFuncao)
+        for i in range(tamFuncao):
+            for j in range(tamFuncao):
+                matrizRes[i,j] = sum(np.multiply(matrizUi[i], matrizUi[j]))
+            vetorF[i] = sum(np.multiply(self.allY, matrizUi[i]))
+        resultado = matrizRes.LUsolve(vetorF)
 
-        exp = sy.sympify(func)
-        simplificado = sy.expand(exp)
+        potencializacao = 1
+        resultado_final = 0
+        for i in range(tamFuncao):
+            resultado_final += round(resultado[i,0], 2) * potencializacao
+            potencializacao *= x
 
-        return f'{simplificado}'
+        return resultado_final
 
 def main():
     # Limpa o arquivo out.txt
@@ -65,7 +68,7 @@ def main():
             listX.append(float(x))
             listY.append(float(y))
         except:
-            resultado = InterpolacaoLagrange(listX, listY).resolve()
+            resultado = MMQ_Discreto(listX, listY).resolve()
             # Escreve os resultados no arquivo out.txt
             with open(os.path.join(dir_path, 'out.txt'), 'a') as arquivo:
                 arquivo.write(str(resultado) + '\n\n')
