6ba9af107b27193da7082a5a92da8fe4448d55cf
laribrito <lary.29.ds@gmail.com>
2024-05-12 08:19:48 -0300

    feat

--- segundo bloco/calculoAreaClass.py
+++ segundo bloco/calculoAreaClass.py
@@ -1,12 +1,19 @@
 from validaParesClass import ValidaPares
-from sympy import integrate, Symbol
+import sympy as sp
 
 class CalculoIntegral(ValidaPares):
     def __init__(self, listX, listY,f=None) -> None:
         super().__init__(listX, listY)
         self.func = f
+        self.result = None
+
+    def integralReal(self):
+        if self.func:
+            x = sp.symbols('x')
+            
+            # Integre a expressão simbólica
+            return sp.integrate(self.func, (x, self.allX[0], self.allX[-1]))
 
     def calcErro(self):
-        if self.func:
-            x = Symbol('x')
-            return integrate(self.func, (x, self.allX[0], self.allX[-1]))
+        if self.result and self.func:
+            return self.integralReal() - self.result
